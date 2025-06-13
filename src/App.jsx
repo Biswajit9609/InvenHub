cd57eee388b4abb90631fa295583e51f2733495c
Biswajit9609 <biswajitchatterjee347@gmail.com>
2025-06-14 03:24:42 +0530

    css updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
     <>
       <div className="landing w-full flex flex-col">
         <Navbar/>
-        <div className="banner flex">
+        <div className="flex flex-col lg:flex-row">
         <div className="illustration-container w-5/12"><img src={image} alt="" /></div>
-        <div className="text-container w-7/12 text-black flex flex-col justify-center items-center">
+        <div className="text-container w-7/12 text-black flex flex-col lg:justify-center lg:items-center">
           <div className="heading font-semibold">
             <h1 className='text-8xl'>Your <span className='font text-red-600'>Store</span></h1>
             <h1 className='text-8xl'> in Your <span className='font text-red-600'>Pocket</span></h1>
