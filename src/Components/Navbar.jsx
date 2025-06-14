61779951ab6a164d7987210ac28d19d9eee69a3f
Biswajit9609 <biswajitchatterjee347@gmail.com>
2025-06-14 12:32:56 +0530

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <>
-        <nav className='navbar w-[90%] bg-[#f8f9fa] flex flex-col lg:flex-row justify-center lg:justify-between items-center lg:gap-0 gap-5 py-10 lg:py-4 lg:px-10 lg:mt-7 rounded-2xl'>
-            <div className="logo-container logo font">
-                <h1>Inven<span className='text-[var(--logo-Red)]'>Hub</span></h1>
-            </div>
-            <div className="nav-list-container flex gap-12 items-center flex-col text-lg lg:flex-row">
-                <a href="" className='nav-link'>Features</a>
-                <a href="" className='nav-link'>Pricing</a>
-                <a href="" className='nav-link'>Login</a>
-                <a href="" className='get-started-btn'>Get Started</a>
-            </div>
-        </nav>
-    </>
-  )
+    <nav className="navbar w-[90%] flex flex-col lg:flex-row justify-between items-center py-6 lg:py-4 lg:px-10 lg:mt-4 rounded-2xl mx-auto">
+      <div className="logo-container logo font text-2xl font-bold">
+        <h1>
+          Inven<span className="text-[var(--logo-Red)]">Hub</span>
+        </h1>
+      </div>
+
+      {/* Hamburger for mobile */}
+      <div
+        className="lg:hidden absolute right-8 top-6 cursor-pointer"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <div className="space-y-1">
+          <span className="block w-6 h-0.5 bg-black"></span>
+          <span className="block w-6 h-0.5 bg-black"></span>
+          <span className="block w-6 h-0.5 bg-black"></span>
+        </div>
+      </div>
+
+      {/* Nav Items */}
+      <div
+        className={`nav-list-container flex-col lg:flex-row gap-6 lg:gap-12 items-center text-lg ${
+          isMenuOpen ? 'flex mt-6' : 'hidden'
+        } lg:flex`}
+      >
+        <a href="#" className="nav-link"><span>Features</span></a>
+        <a href="#" className="nav-link"><span>Pricing</span></a>
+        <a href="#" className="nav-link"><span>Login</span></a>
+        <a href="#" className="get-started-btn">Get Started</a>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
