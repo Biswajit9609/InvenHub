aad4ca529068c254dec8072692bc9d422f4aeda0
Biswajit9609 <biswajitchatterjee347@gmail.com>
2025-06-17 00:59:11 +0530

    1.login page added 2. font updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Landing from './Pages/Landing'
+import LoginPage from './Pages/Login'
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />
+    }
+  ])
   return (
     <>
-        <Landing />
+    <RouterProvider router={router} />
+        {/* <Landing /> */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Landing from './Pages/Landing'
+import Login from './Pages/Login'
 import './index.css'
-import App from './App.jsx'
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+      // children: [
+      //   {
+      //     path: "login",
+      //     element: <Login />
+      //   }
+      // ]
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+  ])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
